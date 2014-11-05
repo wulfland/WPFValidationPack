cbb0abbbc6df24b7a63bf320ce71d7bdf6b9d07b
Mike Kaufmann <wulfland@hotmail.com>
2014-11-05 15:40:58 +0100

    Added rest

---
+++ WPFValidationPack/WPFValidationPack/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+using System;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WPFValidationPack")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WPFValidationPack")]
+[assembly: AssemblyCopyright("Copyright © Michael Kaufmann 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f0fad26c-9f5b-473a-b943-3f7f98c61c92")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
+[assembly: CLSCompliant(true)]
+[assembly: InternalsVisibleTo("WPFValidationPack.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010081970b18502ce10a7efde1413c0c585a54ed4ba4f72a406eb29050a19c00277bf56eee15a9515fb9cbeaddb13f1179637e2729025115a3ef9f5465b7ab35302e2e539613dd5b77bc8512ad31fc03ba033a50c3d6b570f8bf42c49f69b2b4fe392004fc5287bcd77f5b040d1c1df2c3ad956a43679d85b97af7f26aa167b5edb3")]
---
+++ WPFValidationPack/WPFValidationPack/ValidationBase.cs
@@ -0,0 +1,81 @@
+﻿namespace WPFValidationPack
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+using System.Linq.Expressions;
+
+    public abstract class ValidationBase : ICanValidate, INotifyPropertyChanged
+    {
+        public abstract IEnumerable<IValidator> Validators { get;  }
+
+        public void Validate()
+        {
+            ValidationService.Validate(this);
+        }
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            return ValidationService.GetErrors(this, propertyName);
+        }
+
+        public bool HasErrors
+        {
+            get
+            {
+                return ValidationService.HasErrors(this);
+            }
+        }
+
+        public void Validate(string propertyName)
+        {
+            ValidationService.Validate(this, propertyName);
+        }
+
+        public void Validate<T>(Expression<Func<T>> property)
+        {
+            var lambda = property as LambdaExpression;
+
+            if (lambda == null)
+                throw new ArgumentException("Invalid lambda expression.");
+
+            var propertyName = GetPropertyName(lambda);
+
+            Validate(propertyName);
+        }
+
+        public void RaiseErrorsChangedEvent(DataErrorsChangedEventArgs e)
+        {
+            if (ErrorsChanged != null)
+                ErrorsChanged(this, e);
+        }
+
+
+        public void RaisePropertyChangedEvent(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, e);
+        }
+
+        private string GetPropertyName(LambdaExpression lambda)
+        {
+            if (lambda.Body is UnaryExpression)
+            {
+                var unaryExpression = lambda.Body as UnaryExpression;
+                var memberExpression = unaryExpression.Operand as MemberExpression;
+
+                return memberExpression.Member.Name;
+            }
+            else
+            {
+                var memberExpression = lambda.Body as MemberExpression;
+                return memberExpression.Member.Name;
+            }
+        }
+    }
+}
